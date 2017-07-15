cb7ebc026d3495d5bcecd329e9a38b11a232df0b
LvmooGroup <kay2u@live.shop.edu.cn>
2017-07-15 17:23:31 +0800

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -2,15 +2,15 @@ export default {
     MAXFileSize: 5,
     CompressAvatarSize: 100,
     revokeMsgTimeLimit: 2*60*1000,
-    inviteLink: 'http://nami.mdzzapp.com/invite/',
+    inviteLink: 'https://chat.lvmoo.com/invite/',
     ImageStyle: '?imageView2/1/w/250/h/169/interlace/1/q/100', //七牛图片处理样式
-    imageDomain: 'http://oj7h98lzb.bkt.clouddn.com',
-    ImageNotFound: 'http://oj7h98lzb.bkt.clouddn.com/404.jpeg',
-    LoadingImage: 'https://ooo.0o0.ooo/2017/01/17/587dd9f639872.gif',
-    ErrorImage: 'http://oj7h98lzb.bkt.clouddn.com/error.svg',
-    defaultGroupAvatar: 'http://oj7h98lzb.bkt.clouddn.com/download.svg',
+    imageDomain: 'https://cdn.lvmoo.com/chat/image',
+    ImageNotFound: 'https://cdn.lvmoo.com/chat/404.jpeg',
+    LoadingImage: 'https://cdn.lvmoo.com/chat/loading.gif',
+    ErrorImage: 'https://cdn.lvmoo.com/chat/error.svg',
+    defaultGroupAvatar: 'https://cdn.lvmoo.com/chat/download.svg',
     ScreenMessageLenght: 15,
-    FileImage: 'http://oj7h98lzb.bkt.clouddn.com/file.png',
+    FileImage: 'https://cdn.lvmoo.com/chat/file.png',
     bgCoordinates: [{
         title: 'Photos',
         coordinate: [-1802,-1804]
